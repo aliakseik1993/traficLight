df39c1f9f5f081e7d46354b24d6812ec26bef90f
aliakseik <koran.aliaksei@yandex.by>
2020-01-26 09:31:11 -0800

    Initial Commit

---
+++ traficLight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  traficLight
+//
+//  Created by Ульяна Сексишмекси on 26.01.2020.
+//  Copyright © 2020 Ульяна Сексишмекси. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
