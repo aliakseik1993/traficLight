2a034a66eea4627cd8ae23e1ca86304e0a575931
aliakseik <koran.aliaksei@yandex.by>
2020-01-27 08:39:42 -0800

    hell Yeah

--- traficLight/ViewController.swift
+++ traficLight/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
-
+enum Colors {
+    case yellow
+    case green
+    case red
+}
 class ViewController: UIViewController {
+
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var startVar: UIButton!
+    var signal = Colors.red
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    redLight.layer.cornerRadius = 70
+    yellowLight.layer.cornerRadius = 70
+    greenLight.layer.cornerRadius = 70
+        startVar.layer.cornerRadius = 15
+    redLight.alpha = 0.3
+    yellowLight.alpha = 0.3
+    greenLight.alpha = 0.3
+        
     }
-
-
+    @IBAction func StartPressed() {
+       startVar.setTitle("NEXT", for: .normal)
+        switch signal {
+        case .red:
+            redLight.alpha = 1
+            greenLight.alpha = 0.3
+            signal = Colors.yellow
+        case .yellow:
+            yellowLight.alpha = 1
+            redLight.alpha = 0.3
+            signal = Colors.green
+        case .green:
+            self.greenLight.alpha = 1
+            yellowLight.alpha = 0.3
+            signal = Colors.red
+            
+//
+//      default:
+//            print("Don`t work")
+        }
+   
 }
-
+}
